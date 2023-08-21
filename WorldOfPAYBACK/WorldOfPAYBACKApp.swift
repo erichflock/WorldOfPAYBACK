919d08291ef6785671b3b71df2e0eec51099bec1
Erich Flock <erich.flock@check24.de>
2023-08-21 17:20:38 +0200

    Create initial project

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Erich.Flock on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
